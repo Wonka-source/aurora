1a638e81e2f351108be2c247aa0b129919a38f5a
Wonka-source <dan.eff117@gmail.com>
2023-03-26 01:10:51 +0000

    fix typo

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ecommerce.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["proj5.herokuapp.com", "localhost"]
 
 
 # Application definition
