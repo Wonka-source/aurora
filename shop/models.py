934d4741c2b654e8bfa690e75b6b7455758956d9
Wonka-source <dan.eff117@gmail.com>
2023-04-11 17:04:34 +0000

    created shop app and added a shop items model

---
+++ shop/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ShopItemsWatches(models.Model):
+    name = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+    price = DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    mechanism_type = models.CharField(max_length=50, choices=(
+        ('quartz', 'Quartz'),
+        ('mechanical', 'Mechanical'),
+        ('automatic', 'Automatic'),
+        ('kinetic', 'Kinetic'),
+        ('digital', 'Digital'),
+        ('solar', 'Solar-powered'),
+        ('smart', 'Smart'),
+        ('other', 'Other'),
+    ))
+    in_stock = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
