7cc2ad40e352fa851690dcb9d4b3104b525b9045
Wonka-source <dan.eff117@gmail.com>
2023-04-12 19:49:46 +0000

    Coustomized admin

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from .models import Watch, Brand
 
 # Register your models here.
-admin.site.register(Watch)
-admin.site.register(Brand)
+
+
+class WatchAdmin(admin.ModelAdmin):
+    list_display = (        
+        'name',
+        'brand',
+        'sku',
+        'price',
+        'image',
+    )
+
+    ordering = ['brand__name', 'name']
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Watch, WatchAdmin)
+admin.site.register(Brand, BrandAdmin)
+
+
