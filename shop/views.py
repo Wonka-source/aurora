8f7f523b6028d5a89f81d864914d620686a42dce
Wonka-source <dan.eff117@gmail.com>
2023-04-12 20:34:29 +0000

    addedd sthe sop app 'siew and template e

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Watch
 
 # Create your views here.
+
+
+def all_watches(request):
+    """A view to show all watches, including sorting and search queries"""
+
+    watches = Watch.objects.all()
+
+    context = {
+        'watches': watches,
+    }
+
+    return render(request, 'shop/watches.html', context)
