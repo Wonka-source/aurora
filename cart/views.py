51517be9f27ac339e76c4807caa3d0f9b08f8008
Wonka-source <dan.eff117@gmail.com>
2023-04-18 19:10:33 +0000

    Added notification Css, shopping cart preview, and additional messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from shop.models import Product
 
@@ -19,12 +19,11 @@ def add_to_cart(request, item_id):
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
-        
+        messages.success(request, f'Added {product.name} to your cart')
     request.session['cart'] = cart
-
-    messages.success(request, f'Added {product.name} to your cart')
 
     return redirect(redirect_url)
 
@@ -32,13 +31,16 @@ def add_to_cart(request, item_id):
 def adjust_cart(request, item_id):
     """Adjust quantity of the specified product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse(view_cart))
@@ -47,11 +49,15 @@ def adjust_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """Remove the item from the shopping cart"""
 
+    product = get_object_or_404(Product, pk=item_id)
+
     try:
         cart = request.session.get('cart', {})
         cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart')
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
