54318952032342a0cabc87add1e30b7b28527ede
Wonka-source <dan.eff117@gmail.com>
2023-04-19 00:55:41 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_live_51Mxy0rCv1VUOGFUZY1QyUtHkFPKLsaTeR9YLDTQy9ETvulMdptrGQqPWz600g3rOWCtXdMnHFjl0NYopc8XjAqIY00d1emRtkA',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
