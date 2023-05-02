b727d838417d856e7f4e8d9343c344ea57d8b6ab
Wonka-source <dan.eff117@gmail.com>
2023-05-02 23:10:59 +0000

    push for deployed testing

--- profiles/models copy.py
+++ profiles/models copy.py
@@ -8,7 +8,6 @@ class CustomerProfile(models.Model):
     A model representing a customer profile, linked to a user account.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_of_birth = models.DateField(null=True, blank=True)
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=80)
